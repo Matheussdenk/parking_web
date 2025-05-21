9e2e98e7393a7ccf7528b21e9355a10b1c4c4118
Matheussdenk <matheussdenk@gmail.com>
2025-05-21 13:09:08 -0300

    corrigindo cagada

--- config.py
+++ config.py
@@ -13,4 +13,3 @@ class Config:
     SECRET_KEY = 'sua_chave_secreta'
     SQLALCHEMY_DATABASE_URI = 'postgresql://parking_db_e16y_user:vlcirPPqpzXt3KNefXn1pSyuoQuHhVvM@dpg-d0l1ghpr0fns7391uumg-a.oregon-postgres.render.com:5432/parking_db_e16y'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
